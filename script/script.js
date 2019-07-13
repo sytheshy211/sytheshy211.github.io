2c26c65d44edc5910ee02d590b638b9078cc7961
Thanh Sang <sytheshy211@gmail.com>
2019-07-13 16:36:19 +0700

    update

--- script/script.js
+++ script/script.js
@@ -6,4 +6,13 @@ function appear() {
     document.getElementById("time").style.animationName='slowly3';
     document.getElementById("photo").style.animationName='slowly2';
     document.getElementById("play").style.visibility='hidden';
+    document.getElementById("memo1").style.visibility='visible';
+    document.getElementById("memo1").style.animationName='slowly3';
+    document.getElementById("memo2").style.visibility='visible';
+    document.getElementById("memo2").style.animationName='slowly3';
+    document.getElementById("memo3").style.visibility='visible';
+    document.getElementById("memo3").style.animationName='slowly3';
+
+
+
 }
