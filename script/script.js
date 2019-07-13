3b49761b935960120cbe1b740e0269e3439671dd
Thanh Sang <sytheshy211@gmail.com>
2019-07-13 14:43:25 +0700

    second commit

--- script/script.js
+++ script/script.js
@@ -1,11 +1,9 @@
-function HOCSINH(name, class1 , age) {
-    this.NAME = name;
-    this.AGE = age;
-    this.CLASS = class1;
-    this.FULL_INF = function() {
-        return this.NAME + ' ' + this.CLASS +' ' + this.AGE;
-    }
+function appear() {
+    document.getElementById("people").style.visibility='visible';
+    document.getElementById("photo").style.visibility='visible';
+    document.getElementById("time").style.visibility='visible';
+    document.getElementById("people").style.animationName='slowly1';
+    document.getElementById("time").style.animationName='slowly3';
+    document.getElementById("photo").style.animationName='slowly2';
+    document.getElementById("play").style.visibility='hidden';
 }
-var thanhsang = new HOCSINH("Nguyen Thanh Sang" , "12/3", 18);
-var sanginf = thanhsang.FULL_INF();
-alert(sanginf);
